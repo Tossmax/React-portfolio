a3b3e04b6aeafa367939ba36712148dabb7900c2
Ahmed <tossmax61@gmail.com>
2022-06-22 05:19:03 -0700

    Portfolio web app

---
+++ portfolio-app/src/components/WorkCarddata.js
@@ -0,0 +1,27 @@
+import pro1 from "../assets/12.jpg";
+import pro2 from "../assets/13.png";
+import pro3 from "../assets/14.png";
+
+const ProjectCardData = [
+    {
+        imgsrc: pro1,
+        title: "Ecommerce Website Design",
+        text: "Thriving facebook community and top-ranked podcast, Ahmed inspires a grounded, tangible and self-affirming sense of 'wow! I really can do this' for over 100,000 online entrepreneurs. Best-selling markrting courses",
+        view: "https://www.figma.com/proto/GihQFPyMLLREVlM0yKAuZS/Oluwatosin?node-id=327%3A4276&scaling=scale-down&page-id=310%3A5218&starting-point-node-id=327%3A4276"
+    },
+    {
+        imgsrc: pro3,
+        title: "A Website Design",
+        text: "Thriving facebook community and top-ranked podcast, Ahmed inspires a grounded, tangible and self-affirming sense of 'wow! I really can do this' for over 100,000 online entrepreneurs. Best-selling markrting courses",
+        view: "https://preview.webflow.com/preview/virtual-reality-00a1f0?utm_medium=preview_link&utm_source=designer&utm_content=virtual-reality-00a1f0&preview=596a23cffc890cfd7a6881fefeba2d30&workflow=preview"
+    },
+    {
+        imgsrc: pro2,
+        title: "React Toastiy-app",
+        text: "Thriving facebook community and top-ranked podcast, Ahmed inspires a grounded, tangible and self-affirming sense of 'wow! I really can do this' for over 100,000 online entrepreneurs. Best-selling markrting courses",
+        view: "https://codesandbox.io/s/ip480"
+    }
+]
+
+
+export default ProjectCardData;
